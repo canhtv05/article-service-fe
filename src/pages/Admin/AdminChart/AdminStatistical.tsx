3e5d0536096c0ebd7003b283a81cfe0e8709b7b0
canhtv05 <canhtvph51036@gmail.com>
2025-06-10 17:23:13 +0700

    feat(pr-chart): Integrate API into page, filter chart by select box

--- src/pages/Admin/AdminChart/AdminStatistical.tsx
+++ src/pages/Admin/AdminChart/AdminStatistical.tsx
@@ -1,10 +1,37 @@
 import PieChart from '@/components/PieChart';
 import Statistical from '@/components/Statistical';
+import { ChartConfig } from '@/components/ui/chart';
 import { chartData } from '@/constant';
 import { StatisticalItemType } from '@/types';
 import { httpRequest } from '@/utils/httpRequest';
 import { useQuery } from '@tanstack/react-query';
 import { Archive, CircleCheck, FileUp, Send } from 'lucide-react';
+
+const chartConfig = {
+  visitors: {
+    label: 'Visitors',
+  },
+  chrome: {
+    label: 'Chrome',
+    color: 'hsl(var(--chart-1))',
+  },
+  safari: {
+    label: 'Safari',
+    color: 'hsl(var(--chart-2))',
+  },
+  firefox: {
+    label: 'Firefox',
+    color: 'hsl(var(--chart-3))',
+  },
+  edge: {
+    label: 'Edge',
+    color: 'hsl(var(--chart-4))',
+  },
+  other: {
+    label: 'Other',
+    color: 'hsl(var(--chart-5))',
+  },
+} satisfies ChartConfig;
 
 const AdminStatistical = () => {
   const { data, isLoading, error } = useQuery({
@@ -53,7 +80,7 @@ const AdminStatistical = () => {
     <>
       <Statistical statistical={statistical} />
       <div className="rounded-xl md:min-h-min flex-1 border">
-        <PieChart chartData={chartData(statistical)} />
+        <PieChart chartData={chartData(statistical)} chartConfig={chartConfig} />
       </div>
     </>
   );
