2c6c4130e76d66236c176a2ff50b9a84b8416e15
canhtv05 <canhtvph51036@gmail.com>
2025-06-19 22:26:15 +0700

    update(user/list-articles): update api

--- src/components/user/UserFilterListArticleComponent.tsx
+++ src/components/user/UserFilterListArticleComponent.tsx
@@ -1,34 +1,60 @@
-import { Fragment } from 'react';
+import { Fragment, useCallback } from 'react';
 
 import { Label } from '../ui/label';
 import { Input } from '../ui/input';
 import FieldsSelect from '../FieldsSelect';
-import { Status, StatusArticle } from '@/enums';
+import { StatusArticle } from '@/enums';
 import { parseISO } from 'date-fns';
 import { DateRange } from 'react-day-picker';
 import { DateRangePicker } from '../DateRangePicker';
 import { useUserListArticlesContext } from '@/contexts/context/user/UserListArticlesContext';
+import { useQuery } from '@tanstack/react-query';
+import { httpRequest } from '@/utils/httpRequest';
+
+type Campaign = {
+  name: string;
+};
+
+type CampaignListResponse = Campaign[];
 
 const UserFilterListArticleComponent = () => {
   const context = useUserListArticlesContext();
 
+  const { data: campaigns } = useQuery<CampaignListResponse>({
+    queryKey: ['/admin/bai-viet/danh-sach-bai-viet'],
+    queryFn: async () => {
+      const response = await httpRequest.get('/dot-bai-viet/danh-sach-dot-bai-viet');
+      return response.data;
+    },
+  });
+
+  const mappedOptionCampaignName = useCallback(
+    () =>
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      campaigns?.map((item: any) => ({
+        value: item.id,
+        label: item.name,
+      })) ?? [],
+    [campaigns],
+  );
+
   if (!context) return;
 
-  const { end_date, campaign_period, title, topic, start_date, status } = context.valueFilter;
+  const { campaignId, endDate, startDate, title, topicName, status } = context.valueFilter;
 
   const dateRange: DateRange | undefined =
-    start_date || end_date
+    startDate || endDate
       ? {
-          from: start_date ? parseISO(start_date) : undefined,
-          to: end_date ? parseISO(end_date) : undefined,
+          from: startDate ? parseISO(startDate) : undefined,
+          to: endDate ? parseISO(endDate) : undefined,
         }
       : undefined;
 
   const handleChangeDate = (range: DateRange | undefined) => {
     context?.setValueFilter((prev) => ({
       ...prev,
-      start_date: range?.from ? range.from.toISOString() : '',
-      end_date: range?.to ? range.to.toISOString() : '',
+      startDate: range?.from ? range.from.toISOString() : '',
+      endDate: range?.to ? range.to.toISOString() : '',
     }));
   };
 
@@ -53,42 +79,39 @@ const UserFilterListArticleComponent = () => {
       </div>
 
       <div className="flex flex-col justify-end">
-        <Label htmlFor="topic" className="font-bold mb-2 leading-5">
+        <Label htmlFor="topicName" className="font-bold mb-2 leading-5">
           Chủ đề:
         </Label>
         <Input
-          id="topic"
+          id="topicName"
           type="text"
           placeholder="Tìm kiếm theo chủ đề"
-          value={topic}
+          value={topicName}
           onChange={(e) => {
             context.setValueFilter((prev) => ({
               ...prev,
-              topic: e.target.value,
+              topicName: e.target.value,
             }));
           }}
         />
       </div>
 
       <div className="flex flex-col justify-end">
-        <Label htmlFor="campaign_period" className="font-bold mb-2 leading-5">
+        <Label htmlFor="campaignId" className="font-bold mb-2 leading-5">
           Đợt viết bài:
         </Label>
         <FieldsSelect
-          id="campaign_period"
+          id="campaignId"
           placeholder="-- Chọn đợt viết bài --"
-          data={[
-            { label: 'Tháng 10 (01/10/2005 - 30/10/2005)', value: 'Tháng 10 (01/10/2005 - 30/10/2005)' },
-            { label: 'Tháng 9 (08/09/2005 - 30/09/2005)', value: 'Tháng 9 (08/09/2005 - 30/09/2005)' },
-          ]}
+          data={mappedOptionCampaignName()}
           label="Đợt viết bài"
           defaultValue={''}
-          value={campaign_period}
+          value={campaignId}
           setValue={(val) => {
-            if (typeof val === 'string' && context && val !== campaign_period) {
+            if (typeof val === 'string' && context && val !== campaignId) {
               context.setValueFilter((prev) => ({
                 ...prev,
-                campaign_period: val,
+                campaignId: val,
               }));
             }
           }}
@@ -103,13 +126,14 @@ const UserFilterListArticleComponent = () => {
           id="status"
           placeholder="-- Chọn trạng thái --"
           data={[
-            { label: 'Tất cả', value: Status.ALL },
             { label: StatusArticle.POSTED, value: StatusArticle.POSTED },
             { label: StatusArticle.NOT_POSTED, value: StatusArticle.NOT_POSTED },
             { label: StatusArticle.PENDING, value: StatusArticle.PENDING },
+            { label: StatusArticle.INACTIVE, value: StatusArticle.INACTIVE },
+            { label: StatusArticle.SendToPR, value: StatusArticle.SendToPR },
+            { label: StatusArticle.APPROVED, value: StatusArticle.APPROVED },
           ]}
           label="Trạng thái"
-          defaultValue={Status.ALL}
           value={status}
           setValue={(val) => {
             if (typeof val === 'string' && context && val !== status) {
